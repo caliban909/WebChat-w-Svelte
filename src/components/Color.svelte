2957b729832296d61d517f3eb1f8e5eb4b6bc194
caliban909 <maximilianberndlindner@gmail.com>
2021-11-10 09:33:49 +0100

    Create Color.svelte

---
+++ src/components/Color.svelte
@@ -0,0 +1,35 @@
+<!--
+    This module handles color selections
+-->
+
+<script lang="ts">
+    export let color: string | null;
+    export let colors: [string, string][] = [
+    ["#0fbddf", "Blue"],
+    ["#9fdf9f", "Green"],
+    ["#8cd9c6", "Turquoise"],
+    ["#af9fdf", "Purple"],
+    ["#df9f9f", "Pink"],
+    ["goldenrod", "Gold"],
+    ["blueviolet", "Nice"],    
+    ];
+</script>
+
+<!--html-->
+<div class="container">
+    {#each colors as [cl, name]}
+        {name} <button class="cell" on:click="{() => color = cl}" /><br />
+    {/each}
+</div>
+
+
+<style lang="scss">
+    .container{
+        display: flex;
+        justify-content: space-evenly;
+    }
+    .cell{
+        height: 2rem;
+        width: 2rem;
+    }
+</style>
